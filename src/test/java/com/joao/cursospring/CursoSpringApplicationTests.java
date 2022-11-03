03cd36d258d89986a12749e72fd246ef8fe70d3c
joaomarcus13 <joaomarcus13@hotmail.com>
2022-11-03 18:08:00 -0300

    init tests

--- src/test/java/com/joao/cursospring/CursoSpringApplicationTests.java
+++ src/test/java/com/joao/cursospring/CursoSpringApplicationTests.java
@@ -1,5 +1,7 @@
 package com.joao.cursospring;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class CursoSpringApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
